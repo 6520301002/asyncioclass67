3c9ddea49f60bd95323dfc0f4eec0a05ace56a39
6520301002 <6520301002@cdti.ac.th>
2024-07-10 09:37:14 +0700

    s

--- assignment4/breakfast02.py
+++ assignment4/breakfast02.py
@@ -1,3 +1,25 @@
 # Concurrently breakfast
 import asyncio
 from time import sleep, time
+
+async def make_coffee():
+    print("coffee: prepare ingridients")
+    sleep(1)
+    print("coffee: waiting...")
+    await asyncio.sleep(5) #2: pause, another
+    print("coffee: ready")
+    
+async def fry_eggs():
+    print("egg: prepare ingridients")
+    sleep(1)
+    print("egg: frying...")
+    await asyncio.sleep(3)
+    print("eggs: ready")
+    
+async def main():
+    start = time()
+    await make_coffee()
+    await fry_eggs()
+    print(f"breakfast is ready in {time()-start} min")
+    
+    asyncio.run(main())
